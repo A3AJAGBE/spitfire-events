b977c2ce95866c4da85a49e718056be084e4fd99
MAC <horlarmihleykan10@gmail.com>
2023-09-21 01:55:49 +0100

    create event operation

--- Event/config.py
+++ Event/config.py
@@ -11,10 +11,15 @@ load_dotenv(".env")
 class App_Config:
     """_summary_
     """
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    # SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = 'dwddwfwdg'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI",
-                                             "sqlite:///test.db")
+
+    SQLALCHEMY_DATABASE_URI = "postgresql://shabzy:1111@localhost:5432/hng"
+
+
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI",
+    #                                          "sqlite:///test.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
 
